b2f1fe377db26552e83728eef6c7e23bf67e83f4
Lesedi <54321296+Light76@users.noreply.github.com>
2022-09-07 23:38:19 +0200

    Add files via upload

---
+++ Les_API2/Les_API2/Controllers/TodoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Les_API2.Models;
+
+namespace Les_API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoesController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public TodoesController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todoes
+        [HttpGet]
+        public IEnumerable<Todo> GetTodoItems()
+        {
+            return _context.TodoItems;
+        }
+
+        // GET: api/Todoes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTodo([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.TodoItems.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // PUT: api/Todoes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodo([FromRoute] long id, [FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todoes
+        [HttpPost]
+        public async Task<IActionResult> PostTodo([FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.TodoItems.Add(todo);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+            return CreatedAtAction(nameof(GetTodoItems), new { id = todo.Id }, todo);
+
+        }
+
+        // DELETE: api/Todoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        private bool TodoExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
